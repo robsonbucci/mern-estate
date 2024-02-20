f675bd37afb0e4713295a55087844952989add9c
robsonbucci <robsonbucci@hotmail.com>
2024-02-20 17:45:52 -0300

    complete profile page UI

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        'https://media.istockphoto.com/id/1495088043/pt/vetorial/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=S7d8ImMSfoLBMCaEJOffTVua003OAl2xUnzOsuKIwek=',
+        'https://media.istockphoto.com/id/1026502058/pt/vetorial/avatar.jpg?s=612x612&w=0&k=20&c=JTMJyth8R6eutiAtAj9DVi41Tcf6qa3hl4FOpFXx_58=',
       required: true,
     },
   },
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import { useSelector } from 'react-redux';
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>;
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" />
+        <input type="text" placeholder="email" className="border p-3 rounded-lg" />
+        <input type="text" placeholder="password" className="border p-3 rounded-lg" />
+        <button className="bg-slate-700 text-white p-3 rounded-lg rounded-lg hover:opacity-80 disabled:opacity-95">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
